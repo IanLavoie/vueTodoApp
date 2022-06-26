90cb4c8fe5260dd3f19cd9131c2c8c3a2d44cc88
IanLavoie <ianlavoie95@gmail.com>
2022-06-26 11:00:41 -0500

    Removed HelloWorld/updated Nav

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About My Vueitfy Todo App</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium animi doloremque ex repellat distinctio
+      ducimus earum harum sit est, beatae, illo provident laudantium nihil autem minima non quae adipisci fugiat?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium animi doloremque ex repellat distinctio
+      ducimus earum harum sit est, beatae, illo provident laudantium nihil autem minima non quae adipisci fugiat?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium animi doloremque ex repellat distinctio
+      ducimus earum harum sit est, beatae, illo provident laudantium nihil autem minima non quae adipisci fugiat?</p>      
   </div>
 </template>
